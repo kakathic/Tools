adb705d47b606f0b697a8e6a0481eb14588aad59
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-08-31 15:05:14 +0700

    Update kikfox.sh

--- Library/kikfox/kikfox.sh
+++ Library/kikfox/kikfox.sh
@@ -1,2 +1,17 @@
-Taive "https://github.com/kikfox/kikfox/raw/main/bin/kikfox.jar" $HOME/lib/Tools/kikfox.jar
-Tai
+# kakathic
+Xu_install zip
+Xu_install jre
+Xu_install aapt
+Xu_install aapt2
+Taive "https://github.com/kikfox/kikfox/raw/main/bin/$1.jar" $HOME/lib/Tools/$1.jar
+Taive "https://github.com/kakathic/Tools/raw/Vip/Library/$1/$1" $HOME/bin/$1
+mkdir -p $HOME/tmp/prebuilt/linux
+echo 'exec $HOME/bin/aapt "$@"' > $HOME/tmp/prebuilt/linux/aapt
+echo 'exec $HOME/bin/aapt2 "$@"' > $HOME/tmp/prebuilt/linux/aapt2
+echo 'exec $HOME/bin/aapt "$@"' > $HOME/tmp/prebuilt/linux/aapt_64
+echo 'exec $HOME/bin/aapt2 "$@"' > $HOME/tmp/prebuilt/linux/aapt2_64
+cd $HOME/tmp
+zip -qr -9 $HOME/lib/Tools/$1.jar -d prebuilt/windows/* prebuilt/macosx/*
+zip -qr -9 $HOME/lib/Tools/$1.jar prebuilt/*
+[ -e $HOME/lib/Tools/$1.jar ] && echo > $HOME/ck/$1$pb
+rm -fr $HOME/tmp
