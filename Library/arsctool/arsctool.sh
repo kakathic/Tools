6c8cdf6938d6c1233cd99a451c84ca22d1ae58f0
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-08-31 15:21:26 +0700

    Update arsctool.sh

--- Library/arsctool/arsctool.sh
+++ Library/arsctool/arsctool.sh
@@ -1,1 +1,3 @@
-
+# kakathic
+Taive "https://github.com/kakathic/Tools/raw/Vip/Library/$1/$1.jar" $HOME/lib/Tools/$1.jar
+Taive "https://github.com/kakathic/Tools/raw/Vip/Library/$1/$1" $HOME/bin/$1
