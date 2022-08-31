46d8424f03f19187db9452059af3bc643889c36b
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-08-31 12:24:05 +0700

    Update apktool.sh

--- Library/apktool/apktool.sh
+++ Library/apktool/apktool.sh
@@ -1,5 +1,6 @@
 # kakathic
 Xu_install zip
+
 Taive "https://github.com/iBotPeaches/Apktool/releases/download/v${2//_/}/$1$pb.jar" "/data/tools/lib/Tools/$1.jar"
 Taive "https://github.com/kakathic/Tools/raw/Vip/Library/apktool/apktool" "/data/tools/bin/apktool"
 
