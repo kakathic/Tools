2d7a93c62076d887cc7d2db15aea868a9841ba27
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-08-31 11:52:00 +0700

    Update apktool.sh

--- Library/apktool/apktool.sh
+++ Library/apktool/apktool.sh
@@ -1,1 +1,4 @@
 # kakathic
+Taive "https://github.com/iBotPeaches/Apktool/releases/download/v${2//_/}/$1$pb.jar" "/data/tools/Tools/$1.jar"
+Taive "https://github.com/kakathic/Tools/raw/Vip/Library/apktool/apktool" "/data/tools/bin/apktool"
+rm -fr "$Txtmp"
