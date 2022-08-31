6bc74a5232236887d9e36fdd39d4af385af6c397
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-08-31 14:22:07 +0700

    Update apktool.sh

--- Library/apktool/apktool.sh
+++ Library/apktool/apktool.sh
@@ -1,15 +1,15 @@
 # kakathic
 Xu_install zip
 Xu_install jre
-Taive "https://github.com/iBotPeaches/Apktool/releases/download/v${2//_/}/$1$pb.jar" lib/Tools/$1.jar
-Taive "https://github.com/kakathic/Tools/raw/Vip/Library/apktool/apktool" bin/apktool
-mkdir -p tmp/prebuilt/linux
-echo > tmp/prebuilt/linux/aapt
-echo > tmp/prebuilt/linux/aapt2
-echo > tmp/prebuilt/linux/aapt_64
-echo > tmp/prebuilt/linux/aapt2_64
-cd tmp
-zip -qr -9 lib/Tools/$1.jar -d prebuilt/windows/* prebuilt/macosx/*
-zip -qr -9 lib/Tools/$1.jar prebuilt/*
-[ -e lib/Tools/$1.jar ] && echo > ck/$1$pb
-rm -fr tmp
+Taive "https://github.com/iBotPeaches/Apktool/releases/download/v${2//_/}/$1$pb.jar" $HOME/lib/Tools/$1.jar
+Taive "https://github.com/kakathic/Tools/raw/Vip/Library/apktool/apktool" $HOME/bin/apktool
+mkdir -p $HOME/tmp/prebuilt/linux
+echo > $HOME/tmp/prebuilt/linux/aapt
+echo > $HOME/tmp/prebuilt/linux/aapt2
+echo > $HOME/tmp/prebuilt/linux/aapt_64
+echo > $HOME/tmp/prebuilt/linux/aapt2_64
+cd $HOME/tmp
+zip -qr -9 $HOME/lib/Tools/$1.jar -d prebuilt/windows/* prebuilt/macosx/*
+zip -qr -9 $HOME/lib/Tools/$1.jar prebuilt/*
+[ -e $HOME/lib/Tools/$1.jar ] && echo > $HOME/ck/$1$pb
+rm -fr $HOME/tmp
