1ce512e496e2ad40e1487583f59bfd272f701271
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-08-31 13:36:51 +0700

    Update apktool.sh

--- Library/apktool/apktool.sh
+++ Library/apktool/apktool.sh
@@ -1,16 +1,14 @@
 # kakathic
 Xu_install zip
-
-Taive "https://github.com/iBotPeaches/Apktool/releases/download/v${2//_/}/$1$pb.jar" "/data/tools/lib/Tools/$1.jar"
-Taive "https://github.com/kakathic/Tools/raw/Vip/Library/apktool/apktool" "/data/tools/bin/apktool"
-
-mkdir -p $TMPDIR/prebuilt/linux
-echo > $TMPDIR/prebuilt/linux/aapt
-echo > $TMPDIR/prebuilt/linux/aapt2
-echo > $TMPDIR/prebuilt/linux/aapt_64
-echo > $TMPDIR/prebuilt/linux/aapt2_64
-
-cd $TMPDIR
-zip -qr -9 "/data/tools/lib/Tools/$1.jar" -d prebuilt/windows/* prebuilt/macosx/*
-zip -qr -9 "/data/tools/lib/Tools/$1.jar" prebuilt/*
-[ -e "/data/tools/lib/Tools/$1.jar" ] && echo > /data/tools/ck/$1$pb
+Taive "https://github.com/iBotPeaches/Apktool/releases/download/v${2//_/}/$1$pb.jar" lib/Tools/$1.jar
+Taive "https://github.com/kakathic/Tools/raw/Vip/Library/apktool/apktool" bin/apktool
+mkdir -p tmp/prebuilt/linux
+echo > tmp/prebuilt/linux/aapt
+echo > tmp/prebuilt/linux/aapt2
+echo > tmp/prebuilt/linux/aapt_64
+echo > tmp/prebuilt/linux/aapt2_64
+cd tmp
+zip -qr -9 lib/Tools/$1.jar -d prebuilt/windows/* prebuilt/macosx/*
+zip -qr -9 lib/Tools/$1.jar prebuilt/*
+[ -e lib/Tools/$1.jar ] && echo > ck/$1$pb
+rm -fr tmp
