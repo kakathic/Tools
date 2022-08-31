fa5342dbc00a686be7b0943f6a1c20b5d659e945
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-08-31 14:26:55 +0700

    Update jre.sh

--- Library/jre/jre.sh
+++ Library/jre/jre.sh
@@ -1,5 +1,5 @@
 # kakathic
-Taive "https://github.com/kakathic/Tools/raw/Vip/Library/$1/$1-$ARCH.tar.xz" tmp/$1
-tar -xJf tmp/$1 -C ./
-[ -e tmp/$1 ] && echo > ck/$1$pb
-rm -fr tmp/$1
+Taive "https://github.com/kakathic/Tools/raw/Vip/Library/$1/$1-$ARCH.tar.xz" $HOME//$1
+tar -xJf $HOME/tmp/$1 -C $HOME
+[ -e $HOME/tmp/$1 ] && echo > $HOME/ck/$1$pb
+rm -fr $HOME/tmp/$1
