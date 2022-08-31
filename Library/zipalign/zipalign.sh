1d847f5d70c48a82575cbac4ee36d33f207060b6
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-08-31 12:13:16 +0700

    Update zipalign.sh

--- Library/zipalign/zipalign.sh
+++ Library/zipalign/zipalign.sh
@@ -1,2 +1,2 @@
 # kakathic
-Taive "https://github.com/kakathic/Tools/raw/Vip/Library/zipalign/zipalign-$ARCH" "/data/tools/bin/$1"
+Taive "https://github.com/kakathic/Tools/raw/Vip/Library/$1/$1-$ARCH" "/data/tools/bin/$1"
