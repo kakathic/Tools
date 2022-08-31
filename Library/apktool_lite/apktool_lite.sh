3d3496c945ddca52737fbde42eaee5983f4a006e
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-08-31 13:32:53 +0700

    Update apktool_lite.sh

--- Library/apktool_lite/apktool_lite.sh
+++ Library/apktool_lite/apktool_lite.sh
@@ -1,5 +1,5 @@
 # kakathic
-Taive "https://github.com/kakathic/Tools/raw/Vip/Library/$1/$1$pb.zip" "/data/tools/tmp/$1"
-unzip -o "/data/tools/tmp/$1" -d "/data/tools"
-[ -e "/data/tools/tmp/$1" ] && echo > /data/tools/ck/$1$pb
-rm -fr "/data/tools/tmp/$1"
+Taive "https://github.com/kakathic/Tools/raw/Vip/Library/$1/$1$pb.zip" tmp/$1
+unzip -o tmp/$1 -d ./
+[ -e tmp/$1 ] && echo > ck/$1$pb
+rm -fr tmp/$1
