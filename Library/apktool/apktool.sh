413b60ef353a210e4e63cb01017379489b74ab86
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-08-31 12:22:34 +0700

    Update apktool.sh

--- Library/apktool/apktool.sh
+++ Library/apktool/apktool.sh
@@ -1,6 +1,6 @@
 # kakathic
 Xu_install zip
-Taive "https://github.com/iBotPeaches/Apktool/releases/download/v${2//_/}/$1$pb.jar" "/data/tools/Tools/$1.jar"
+Taive "https://github.com/iBotPeaches/Apktool/releases/download/v${2//_/}/$1$pb.jar" "/data/tools/lib/Tools/$1.jar"
 Taive "https://github.com/kakathic/Tools/raw/Vip/Library/apktool/apktool" "/data/tools/bin/apktool"
 
 mkdir -p $TMPDIR/prebuilt/linux
@@ -10,4 +10,4 @@ echo > $TMPDIR/prebuilt/linux/aapt_64
 echo > $TMPDIR/prebuilt/linux/aapt2_64
 
 cd $TMPDIR
-zip -qr -9 "/data/tools/Tools/$1.jar" prebuilt/* -d prebuilt/windows/* prebuilt/macosx/*
+zip -qr -9 "/data/tools/lib/Tools/$1.jar" prebuilt/* -d prebuilt/windows/* prebuilt/macosx/*
