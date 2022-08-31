6eb3fca55fd8f506df4aa7df99a0d0987cc18f21
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-08-31 14:36:13 +0700

    Update apktool.sh

--- Library/apktool/apktool.sh
+++ Library/apktool/apktool.sh
@@ -4,10 +4,10 @@ Xu_install jre
 Taive "https://github.com/iBotPeaches/Apktool/releases/download/v${2//_/}/$1$pb.jar" $HOME/lib/Tools/$1.jar
 Taive "https://github.com/kakathic/Tools/raw/Vip/Library/apktool/apktool" $HOME/bin/apktool
 mkdir -p $HOME/tmp/prebuilt/linux
-echo > $HOME/tmp/prebuilt/linux/aapt
-echo > $HOME/tmp/prebuilt/linux/aapt2
-echo > $HOME/tmp/prebuilt/linux/aapt_64
-echo > $HOME/tmp/prebuilt/linux/aapt2_64
+echo 'exec $HOME/bin/aapt "$@"' > $HOME/tmp/prebuilt/linux/aapt
+echo 'exec $HOME/bin/aapt2 "$@"' > $HOME/tmp/prebuilt/linux/aapt2
+echo 'exec $HOME/bin/aapt "$@"' > $HOME/tmp/prebuilt/linux/aapt_64
+echo 'exec $HOME/bin/aapt2 "$@"' > $HOME/tmp/prebuilt/linux/aapt2_64
 cd $HOME/tmp
 zip -qr -9 $HOME/lib/Tools/$1.jar -d prebuilt/windows/* prebuilt/macosx/*
 zip -qr -9 $HOME/lib/Tools/$1.jar prebuilt/*
