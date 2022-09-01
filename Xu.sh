67185c44a7f1c2acbc9c11bbd0c0a888a9ed501b
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-09-01 18:27:58 +0700

    Update Xu.sh

--- Xu.sh
+++ Xu.sh
@@ -65,6 +65,11 @@ rm -fr $TMPDIR/*
 fi
 }
 
+Delete_all(){
+rm -fr $TOME/files/term/usr/*
+rm -fr $TOME
+}
+
 # Internet
 User="User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0"
 Taive(){
